8eaf081312adf6f5dc5e91a92240be62abaa9f8c
KushagraKatiyar06 <150945125+KushagraKatiyar06@users.noreply.github.com>
2025-03-24 16:29:38 -0400

    New Gallery and Re Formatting

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const body = document.body;
-    const themeToggle = document.getElementById('themeToggle');
+// Dark Mode
+const themeToggleButton = document.querySelector('.theme-toggle');
+const themeText = document.querySelector('#themeToggle');
 
-    function toggleTheme(){
-        body.classList.toggle('dark-theme');
+themeToggleButton.addEventListener('click', function () {
+    document.body.classList.toggle('dark-theme');
 
-        if (body.classList.contains('dark-theme')){
-            themeToggle.textContent = 'Light';
-        } else {
-            themeToggle.textContent = 'Dark';
-        }
+    if (document.body.classList.contains('dark-theme')) {
+        themeText.textContent = 'Light Mode';
+    } else {
+        themeText.textContent = 'Dark Mode';
     }
-
-    themeToggle.addEventListener('click', toggleTheme);
-
 });
 
+// Carousel
+let currentIndex = 0;
+
+function moveSlide(direction) {
+    const slides = document.querySelectorAll('.carousel-item');
+    const totalSlides = slides.length;
+
+    currentIndex += direction;
+    if (currentIndex < 0) {
+        currentIndex = totalSlides - 1;
+    } else if (currentIndex >= totalSlides) {
+        currentIndex = 0;
+    }
+
+    const offset = -currentIndex * 100;
+    document.querySelector('.carousel-images').style.transform = `translateX(${offset}%)`;
+}
